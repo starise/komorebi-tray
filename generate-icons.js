6dccd2958c7d5a9327a2067559f594d947582b35
Andrea Brandi <me@andreabrandi.com>
2024-06-11 21:21:58 +0200

    build: app icons auto-generation

--- generate-icons.js
+++ generate-icons.js
@@ -1,23 +1,49 @@
 const fs = require("fs");
+const path = require("path");
 const icongen = require("icon-gen");
 
-const sourceDir = "images/png/app";
-const destinationDir = "images/ico";
+const pngDirectory = "images/png";
+const icoDirectory = "images/ico";
 
 const options = {
   report: true,
   ico: {
     name: "app",
-    sizes: [16, 24, 32, 48, 64, 128, 256],
+    sizes: [16, 24, 32, 48, 256],
   },
 };
 
-fs.mkdirSync(destinationDir, { recursive: true });
+fs.mkdirSync(icoDirectory, { recursive: true });
 
-icongen(sourceDir, destinationDir, options)
+icongen(pngDirectory + "/app.png", icoDirectory, options)
   .then((results) => {
     console.log(results);
   })
   .catch((err) => {
     console.error(err);
   });
+
+const pngFiles = fs.readdirSync(pngDirectory).filter((file) => {
+  const fullPath = path.join(pngDirectory, file);
+  return fs.statSync(fullPath).isFile() &&
+    path.extname(file).toLowerCase() === ".png";
+});
+
+pngFiles.filter((e) => e !== "app.png").forEach((file) => {
+  const pngFile = path.join(pngDirectory, file);
+  const pngName = path.basename(file, ".png");
+  const options = {
+    report: true,
+    ico: {
+      name: pngName,
+      sizes: [16, 24, 32, 48],
+    },
+  };
+  icongen(pngFile, icoDirectory, options)
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
